adcb515d9fdbc511603f9cb4f7a99db07912ff8b
Jordan Burger <jordanburger@Jordans-MacBook-Pro.local>
2020-04-03 13:56:08 -0400

    finishing tutorial

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -29,11 +29,14 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
-    def create_app():
-        app = ...
-    # existing code omitted
-
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
